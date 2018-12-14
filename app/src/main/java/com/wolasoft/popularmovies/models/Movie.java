d4534d34cace2e27104033a83b9e603fae33d050
wolasoft <dev.wolasoft@gmail.com>
2018-12-15 00:55:07 +0100

    feat: add movie data model

---
+++ app/src/main/java/com/wolasoft/popularmovies/models/Movie.java
@@ -0,0 +1,88 @@
+package com.wolasoft.popularmovies.models;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Movie implements Parcelable {
+
+    private String originalTitle;
+    private String thumbnailUrl;
+    private float rating;
+    private String synopsis;
+    private String releaseDate;
+
+    public Movie(Parcel parcel) {
+        this.originalTitle = parcel.readString();
+        this.thumbnailUrl = parcel.readString();
+        this.rating = parcel.readFloat();
+        this.synopsis = parcel.readString();
+        this.releaseDate = parcel.readString();
+    }
+
+    public String getOriginalTitle() {
+        return originalTitle;
+    }
+
+    public void setOriginalTitle(String originalTitle) {
+        this.originalTitle = originalTitle;
+    }
+
+    public String getThumbnailUrl() {
+        return thumbnailUrl;
+    }
+
+    public void setThumbnailUrl(String thumbnailUrl) {
+        this.thumbnailUrl = thumbnailUrl;
+    }
+
+    public float getRating() {
+        return rating;
+    }
+
+    public void setRating(float rating) {
+        this.rating = rating;
+    }
+
+    public String getSynopsis() {
+        return synopsis;
+    }
+
+    public void setSynopsis(String synopsis) {
+        this.synopsis = synopsis;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(originalTitle);
+        dest.writeString(thumbnailUrl);
+        dest.writeFloat(rating);
+        dest.writeString(synopsis);
+        dest.writeString(releaseDate);
+    }
+
+    static final Parcelable.Creator<Movie> CREATOR = new Parcelable.Creator<Movie>() {
+
+        @Override
+        public Movie createFromParcel(Parcel source) {
+            return new Movie(source);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[0];
+        }
+    };
+}
